91d9e8b371adfd4c565d98907c7a8aa4bdf4f494
KunChi <kuo23201199@gmail.com>
2019-09-24 17:36:28 +0800

    Modify agruments comment

--- BCRA_run.py
+++ BCRA_run.py
@@ -18,14 +18,14 @@ def getExmpleData(bcra):
 
 def getArgument():
     ID = sys.argv[1]
-    T1 = sys.argv[2] #age, key_name = age
-    T2 = sys.argv[3] #乳房攝影時年齡, 
-    N_Biop = sys.argv[4] #乳房切片次數 key_name = times
-    HypPlas = sys.argv[5] #key_name = type, 若為atypical為1,no=0,unk=99
-    AgeMen = sys.argv[6] #初經年齡, key_name = mena_age
-    Age1st = sys.argv[7] #第一次懷孕年齡, key_name = firstp_age
+    T1 = sys.argv[2] #填表人，目前年齡
+    T2 = sys.argv[3] #切片時的年齡
+    N_Biop = sys.argv[4] #乳房切片次數，unk=99
+    HypPlas = sys.argv[5] #若為atypical為1,no=0,unk=99
+    AgeMen = sys.argv[6] #初經年齡, unk=99
+    Age1st = sys.argv[7] #第一次懷孕年齡, 未產=98 , unk=99
     N_Rels = sys.argv[8] #一等親得乳癌數量。父母兄弟姊妹
-    Race = sys.argv[9] # 1=白人, 11=華人。key_name = racial
+    Race = sys.argv[9] # 1=白人, 11=華人
     
     return ID, T1, T2, N_Biop, HypPlas, AgeMen, Age1st, N_Rels, Race
 
